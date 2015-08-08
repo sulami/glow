98c8ed2d17fdc473e5f9632934161ebd410ae433
Robin Schroer <sulami@peerwire.org>
2015-08-08 19:41:45 +0200

    switch to game mode and display a simple clock

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,13 @@ module Main where
 
 import           Graphics.Gloss
 
-main = do display (InWindow "Hello, World!"
+main = do play (InWindow "Hello, World!"
                             (1024, 768)
                             (10, 10) )
             white
-            picture
-
-picture = Text "Hello, World!"
+            60
+            0
+            (Text . show)
+            (\_ w -> w)
+            (\t w -> w + t)
 

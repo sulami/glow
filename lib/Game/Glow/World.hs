efab1cef6820f009d86f076c8407baf99da9660b
Robin Schroer <sulami@peerwire.org>
2015-08-08 22:18:50 +0200

    introduce a player datatype

--- lib/Game/Glow/World.hs
+++ lib/Game/Glow/World.hs
@@ -4,19 +4,29 @@ module Game.Glow.World (
   step
 ) where
 
--- | The world datatype. Currently only holds a single float that displays the
--- time.
-data World = World Float
+-- | The world datatype. Currently only holds a player.
+data World = World {
+  player :: Player
+}
 
 instance Show World where
-  show (World f) = show f
+  show (World p) = show p
 
 -- | Create an inital world state.
 initalWorld :: World
-initalWorld = World 0
+initalWorld = World $ initalPlayer
 
 -- | Advance the world for the next frame, using the time passed since the last
 -- one.
 step :: Float -> World -> World
-step delta (World w0) = World $ w0 + delta
+step delta (World w0) = World w0
+
+-- | The player datatype. Currently only holds the position.
+data Player = Player {
+  position :: (Float, Float)
+} deriving (Show)
+
+-- | Create the inital player data.
+initalPlayer :: Player
+initalPlayer = Player $ (0, 0)
 

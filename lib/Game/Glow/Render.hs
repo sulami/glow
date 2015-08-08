b520589284c8062eeade9fec18eafba3fd37ad6e
Robin Schroer <sulami@peerwire.org>
2015-08-08 22:18:36 +0200

    adapt the colors

--- lib/Game/Glow/Render.hs
+++ lib/Game/Glow/Render.hs
@@ -2,11 +2,19 @@ module Game.Glow.Render (
   render
 ) where
 
-import           Graphics.Gloss.Data.Picture (Picture, text)
+import           Graphics.Gloss.Data.Color (Color, makeColor)
+import           Graphics.Gloss.Data.Picture (
+  Picture, color, scale, text, translate
+  )
 
 import           Game.Glow.World (World)
 
 -- | Render the world to a picture we can draw.
 render :: World -> Picture
-render = text . show
+render = color devColor . scale 0.2 0.2 . translate (-1024) 0 . text . show
+
+-- | This is the color for use by dev overlays. It is yellow with an alpha of
+-- 0.8.
+devColor :: Color
+devColor = makeColor 1 0.67 0 0.8
 
--- src/Main.hs
+++ src/Main.hs
@@ -11,7 +11,7 @@ import           Game.Glow.World (initalWorld, step)
 main = do play (InWindow ("Glow " ++ showVersion version)
                             (1024, 768)
                             (10, 10) )
-            white                                         -- background
+            black                                         -- background
             60                                            -- fps
             initalWorld                                   -- inital state
             render                                        -- rendering function

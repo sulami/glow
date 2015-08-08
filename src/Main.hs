c188127a038a5f5593ead2803135765b7e92bcf4
Robin Schroer <sulami@peerwire.org>
2015-08-08 21:21:37 +0200

    some beauty fixes

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,19 @@
 module Main where
 
+import           Paths_glow (version)
+import           Data.Version (showVersion)
 import           Graphics.Gloss
 
 import           Game.Glow.Input (handleInput)
 import           Game.Glow.Render (render)
 
-main = do play (InWindow "Hello, World!"
+main = do play (InWindow ("Glow " ++ showVersion version)
                             (1024, 768)
                             (10, 10) )
-            white
-            60
-            0
-            render
-            handleInput
-            (\t w -> w + t)
+            white                                         -- background
+            60                                            -- fps
+            0                                             -- inital state
+            render                                        -- rendering function
+            handleInput                                   -- input handler
+            (\t w -> w + t)                               -- world advancement
 

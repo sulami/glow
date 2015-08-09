df0dd6249346d5bf15ef2394a1caa46b5f762108
Robin Schroer <sulami@peerwire.org>
2015-08-09 16:56:00 +0200

    move the ball with each frame

--- lib/Game/Glow/World.hs
+++ lib/Game/Glow/World.hs
@@ -15,9 +15,14 @@ import           Graphics.Gloss.Data.Picture (
   Picture (Pictures), circle, polygon, translate
   )
 
--- | The global world state. This holds all the state the game has.
+-- | The global world state. This holds all the state the game has. Some of the
+-- sprites have fixed, hardcoded positions in the list:
+--
+-- > 0   - Ball
+-- > 1-4 - Platforms
 data World = World {
-  sprites :: [Sprite] -- ^ All currently present sprites
+  sprites :: [Sprite], -- ^ All currently present sprites
+  ballSpeed :: (Float, Float) -- ^ The speed of the ball along both axes
 } deriving (Show)
 
 -- | Create an inital world state.
@@ -29,6 +34,7 @@ initalWorld = World [ Sprite (circle 12.5) (0,0) (25,25), -- The ball
                       Sprite (makeBox ( 20,100)) (-210,- 50) ( 20,100),
                       Sprite (makeBox ( 20,100)) ( 190,- 50) ( 20,100)
                       ]
+                    (50,50) -- Ball speed
 
 -- | Create a single picture from a world.
 drawWorld :: World -> Picture
@@ -37,7 +43,18 @@ drawWorld w = Pictures $ map drawSprite $ sprites w
 -- | Advance the world for the next frame, using the time passed since the last
 -- one.
 step :: Float -> World -> World
-step delta w0 = w0
+step delta w0 = moveBall delta w0
+
+-- | Modify the world to move the ball according to the current ballspeed. Just
+-- like 'step', it needs the time passed since the last frame to be
+-- fps-independent.
+moveBall :: Float -> World -> World
+moveBall dl w0 = let b0 = head $ sprites w0
+                     (b0x,b0y) = pos b0
+                     (bsx,bsy) = ballSpeed w0
+                     b1p = (dl*bsx + b0x, dl*bsy + b0y)
+                     b1 = Sprite (pic b0) b1p (size b0)
+                  in World (b1 : tail (sprites w0)) (ballSpeed w0)
 
 -- | Any entity, like enemies. Comparable to classic sprites.
 data Sprite = Sprite {
